b05fb980a7f58bc500aa9704c41f632c332d2b4a
Tomas Cerskus <tomas.cerskus@gmail.com>
2015-04-17 15:20:09 -0400

    More cleanup

--- test_bitmap.py
+++ test_bitmap.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import rrt
 from problem import BitmapProblem
 
@@ -7,7 +9,7 @@ from PIL import Image
 if __name__ == '__main__':
 	# Problem
 	problem = BitmapProblem(Image.open("./benchmarks/slit_500x500_offset_init80x250_goal420x250.png"), (80, 250), (420,250), 20)
-	#problem = BitmapProblem(Image.open("./benchmarks/slit_500x500_centre_init80x250_goal420x250.png"), (80, 250), (420,250), 20)
+	# problem = BitmapProblem(Image.open("./benchmarks/slit_500x500_centre_init80x250_goal420x250.png"), (80, 250), (420,250), 20)
 	# problem = BitmapProblem(Image.open("./benchmarks/twoslits_500x500_init80x250_goal420x250.png"), (80, 250), (420,250), 20)
 	# problem = BitmapProblem(Image.open("./benchmarks/maze_500x500_init147x29_goal470x430.png"), (147, 29), (470,430), 20)
 	# problem = BitmapProblem(Image.open("./benchmarks/block_500x500_init80x250_goal420x250.png"), (80, 250), (420,250), 20)
